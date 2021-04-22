ac85d659fc09b6572c384708f4dc805c1506b552
Muhammed Rahif <rahifpalliyalil@gmail.com>
2021-04-22 17:41:05 +0530

    First commit

---
+++ app2.js
@@ -0,0 +1,12 @@
+var typed = new Typed("#main-title", {
+  strings: [
+    "Freelancer.",
+    "Web Designer.",
+    "Web Developer.",
+    "Mobile Developer.",
+  ],
+  typeSpeed: 120,
+  loop: true,
+  backSpeed: 40,
+});
+
